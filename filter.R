e665b36938475cd97766f6a3de765274515c3b24
Sergey Abakumoff <s.abakumoff@gmail.com>
2016-10-30 09:34:26 +0100

    completed the research

--- filter.R
+++ filter.R
@@ -3,7 +3,7 @@ library("dplyr")
 deps<-read.csv("react_deps.csv")
 repos<-read.csv("repos.csv")
 tidy_repos<-deps %>% inner_join(repos)
-temp<-out %>% select(dependency) %>% mutate(name=dependency) %>% select(name)
+temp<-tidy_repos %>% select(dependency) %>% mutate(name=dependency) %>% select(name)
 tidy_repos<-tidy_repos %>% anti_join(temp) %>% unique()
 
 library("tidyr")
@@ -21,3 +21,16 @@ outliers<-num_of_deps %>%
   arrange(desc(total_deps)) %>% head(20) %>%
   mutate(link=paste("https://github.com",repo, sep="/")) %>%
   select(link, dependencies, devDependencies)
+
+runtime_deps<-tidy_repos %>% filter(isDev==0) %>% 
+  select(dependency) %>% count(dependency, sort=TRUE) %>% 
+  head(100) %>% mutate(dependency = reorder(dependency, n))
+
+design_time_deps_top_100<-tidy_repos %>% filter(isDev==1) %>% 
+  select(dependency) %>% count(dependency, sort=TRUE) %>% head(100) %>% 
+  mutate(dependency = reorder(dependency, n))
+
+
+samples<-tidy_repos %>% select(repo, stars) %>% group_by(repo, stars) %>% 
+  count() %>% ungroup() %>% select(-n) %>% arrange(desc(stars)) %>% head(100) %>% 
+  mutate(repo = paste("https://github.com", repo, sep="/"))
